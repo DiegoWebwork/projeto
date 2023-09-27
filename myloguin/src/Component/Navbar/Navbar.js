e340ca053e0da38c87b9fc9689ba5dda55a70b61
Diego <e_guinho@hotmail.com>
2023-09-27 16:24:17 -0300

    images

---
+++ myloguin/src/Component/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./navbar.css"
+export default function Navbar(){
+    <div className='mainNavbar'>
+        <div><p>Social</p></div>
+        <div><img src="" alt=""/> <input type="text" name="" id=""/></div>
+        <div><div><img src="" alt=""/><img src="" alt=""/><div><img src="" alt=""/><p>Catherin</p></div></div></div>
+    </div>
+}
